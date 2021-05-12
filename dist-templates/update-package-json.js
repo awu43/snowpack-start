611ebbbf0dea5444b4dcd724da0fed71d25df27c
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-05-12 14:16:30 -0700

    Updated dist templates package.json files

---
+++ dist-templates/update-package-json.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+const fse = require("fs-extra");
+
+const SOURCE_TEMPLATES = require("../src-templates").SOURCE_PATHS;
+const DIST_TEMPLATES = require(".");
+
+for (const template of SOURCE_TEMPLATES.keys()) {
+  const srcPackageJson = fse.readFileSync(
+    path.join(SOURCE_TEMPLATES.get(template), "package.json"), "utf8"
+  );
+  fse.writeFileSync(
+    path.join(DIST_TEMPLATES.get(template), "package.json"), srcPackageJson
+  );
+}
