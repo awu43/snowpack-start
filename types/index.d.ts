db1f13a31f843f2415fa2906e9b60f58bda1027a
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-06-01 17:58:30 -0700

    Moved js-frameworks types to index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,24 @@
 type LockedMap<K, V> = Omit<Map<K, V>, "get" | "set">;
 
+// js-frameworks
+interface JsFrameworkData {
+  prodPackages: string[];
+  devPackages: string[];
+  tsPackages: string[];
+  wtrPackages: string[];
+  plugins: string[];
+}
+
+type JsFrameworkMap = LockedMap<string, JsFrameworkData> & {
+  get(K: string): JsFrameworkData;
+};
+
 // dist-files and dist-templates
 type DistPathMap = LockedMap<string, string> & {
   get(K: string): string;
 }
 
+// index
 interface PackageJson {
   private: boolean;
   scripts: { [key: string]: string };
