8a29b6b919819715ce70ef65ad4ccc488eec625d
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-06-03 22:28:39 -0700

    Updated source vue templates
    No changes to dist templates made

--- src-templates/vue/snowpack.config.mjs
+++ src-templates/vue/snowpack.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
-module.exports = {
+export default {
   mount: {
     public: {url: '/', static: true},
     src: {url: '/dist'},
   },
-  plugins: [
-    '@snowpack/plugin-vue',
-    '@snowpack/plugin-vue/plugin-tsx-jsx.js',
-    '@snowpack/plugin-dotenv',
-  ],
+  plugins: ['@snowpack/plugin-vue', '@snowpack/plugin-dotenv'],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
